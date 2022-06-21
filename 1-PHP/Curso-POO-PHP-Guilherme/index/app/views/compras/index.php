d36994306f793952f3e7a341b7d0071259c6d280
Carlos Craaj <carloscraaj@gmail.com>
2022-06-21 00:37:53 -0300

    20/06/2022

--- 1-PHP/Curso-POO-PHP-Guilherme/index/app/views/compras/index.php
+++ 1-PHP/Curso-POO-PHP-Guilherme/index/app/views/compras/index.php
@@ -1,23 +1,32 @@
 <?php $this->tituloPagina = "Lista de Compras" ?>
 
 <?php $this->layout('layout.topo'); ?>
-        <h2>Lista de Compras</h2>
+    
+        <div class="row">
+            <h2>Lista de Compras</h2>
+        </div>
 
-        <a href="/compras/adicionar">Criar</a>
-        <ul>
-            <?php foreach ($this->modelo as $key => $value): ?>
-                <li>
-                    <?php echo $value['titulo'] ?> - <?php echo $value['desc'] ?>
+        <div class="row">
+            <a href="/compras/adicionar">Criar</a>
+        </div>
+        <div class="row">
+            <ul>
+                <?php foreach ($this->modelo as $key => $value): ?>
+                    <li>
+                        <?php echo $value['titulo'] ?> - <?php echo $value['desc'] ?>
+                        
+                        <form action="/compras/deletar/<?php echo $value['id'] ?>" method="post">
+                            <a href="/compras/detalhe/<?php echo $value['id'] ?>"><button class="btn btn-primary" type="button">Detalhe</button></a>
+                            <a href="/compras/editar/<?php echo $value['id'] ?>"><button class="btn btn-primary" type="button">Editar</button></a>
+                            <input type="hidden" name="_METHOD" value="delete">
+                            <button class="btn btn-primary">Deletar</button>
+                        </form>
                     
-                    <form action="/compras/deletar/<?php echo $value['id'] ?>" method="post">
-                        <a href="/compras/detalhe/<?php echo $value['id'] ?>"><button type="button">Detalhe</button></a>
-                        <a href="/compras/editar/<?php echo $value['id'] ?>"><button type="button">Editar</button></a>
-                        <input type="hidden" name="_METHOD" value="delete">
-                        <button>Deletar</button>
-                    </form>
-                
-                </li>
-            <?php endforeach ?>
-        </ul>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        
+        
+    </div>    
 
 <?php $this->layout('layout.rodape'); ?>
--- 1-PHP/Curso-POO-PHP-Guilherme/index/app/views/layout/topo.php
+++ 1-PHP/Curso-POO-PHP-Guilherme/index/app/views/layout/topo.php
@@ -3,5 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <title><?php echo isset($this->tituloPagina) ? \Config\SiteInfo::$nome." | ".$this->tituloPagina : \Config\SiteInfo::$nome; ?></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    
     </head>
     <body>
+        <?php $this->layout('layout.nav'); ?>
+        <div class="container">
